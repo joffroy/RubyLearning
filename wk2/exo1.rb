ca69ff37f794bf4364f57c3d8b525f191c067256
Cedric Joffroy <cedric.joffroy@gmail.com>
2013-07-10 14:48:26 +0200

    create week2 folder and add exo1

---
+++ wk2/exo1.rb
@@ -0,0 +1,8 @@
+s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
+# Initialize a counter
+counter=1
+# Split the string and iterate on all elements
+s.split("\n").each do |line|
+  puts "Line #{counter}: #{line}"
+  counter=counter+1
+end
